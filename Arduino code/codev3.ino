bf79b0d49dbc6db013e2e0d2d581209d5efa3d98
B-asmala <basmalabayoumi@gmail.com>
2024-05-16 22:59:29 +0200

    new arduino code

--- Arduino code/codev3.ino
+++ Arduino code/codev3.ino
@@ -5,6 +5,15 @@
 #define echoPin2  11 // controlled by cube
 #define trigPin2  10
 
+#define app_pin 3
+#define sensor_pin 4
+#define mid_pin 2
+
+
+bool from_app = false; 
+bool from_sensor = false;
+
+int i = 0;
 
 // sum for computing the average raw sensor value
 float d1 = 0;
@@ -15,8 +24,7 @@ float distance = 0;
 // motor parameters
 #include <Servo.h>
 Servo servo_motor; 
-int servoMotorPin = 9;        // the servo motor is attached to the 9th Pulse Width Modulation (PWM)Arduino output
-
+int servoMotorPin = 9;        
 
 
 
@@ -26,16 +34,15 @@ float elapsedTime, time, timePrev;        //Variables for time control
 float distance_previous_error, distance_error;
 float PID_p, PID_i, PID_d, PID_total;
 
-float kp=10;            //0.00001 0.00001 25     0.03 0.000001 0       // proportional control 
-float ki=0;                         // integral control
-float kd=0;                        // derivative control
+float kp=4.8;            
+float ki=0.1;                         
+float kd=430;                       
      
-       // discretization constant, that is equal to the total control loop delay, the number 32 is obtained by measuring the time it takes to execute a single loop iteration                         
 float servoAngle;
 
 
-
 float measure_1 (void);
+float measure_2 (void);
 
 void setup() 
 {
@@ -48,6 +55,12 @@ void setup()
   pinMode(trigPin2, OUTPUT);
   pinMode(echoPin2, INPUT);
 
+  pinMode(app_pin, OUTPUT);
+  pinMode(sensor_pin, OUTPUT);
+  pinMode(mid_pin, OUTPUT);
+  pinMode(12, INPUT_PULLUP);
+  digitalWrite(mid_pin, HIGH);
+
   servo_motor.attach(servoMotorPin);
   servo_motor.write(90);
   delay(200);
@@ -58,6 +71,38 @@ void loop()
 {
 //  unsigned long startTime = micros(); // this is used to measure the time it takes for the code to execute
   // obtain the sensor measurements
+
+  if(digitalRead(12) == LOW){
+    digitalWrite(2 + i, LOW);
+    i ++;
+    i %= 3;
+    digitalWrite(2 + i, HIGH);
+
+    if(i == 0){
+      from_app = false;
+      from_sensor = false;
+      setpoint = 20;
+
+    }else if(i == 1){
+
+      from_app = true;
+
+
+    }else if(i == 2){
+      from_app = false;
+      from_sensor = true;
+
+    }
+
+
+    while(digitalRead(12) == LOW){
+      delay(200);
+    }
+
+  }
+
+
+
   if (millis() > time+period)
   {
     time = millis();    
@@ -68,12 +113,18 @@ void loop()
     distance = (d3 + d2 + d1) / 3;  
 
 
-    // distance = 0;
-    // for(int i = 0; i < 10; i ++){
-    //   distance += measure_1();
-    //   delayMicroseconds(10);
-    // }
-    // distance /= 10;
+
+    if(from_sensor){
+            setpoint = measure_2();
+
+    }
+
+    if(from_app){
+          if (Serial.available() > 0) {
+          int serialMessage = Serial.parseInt();
+          setpoint = serialMessage;
+  }
+    }
 
     distance_error = setpoint - distance;   
     PID_p = kp * distance_error;
@@ -89,21 +140,21 @@ void loop()
       PID_i = 0;
     }
   
-    PID_total = PID_p;  
+    PID_total = PID_p + PID_d + PID_i;  
     PID_total = map(PID_total, -150, 150, 0, 150);
   
-    if(PID_total < 40){PID_total = 40;}
-    if(PID_total > 90) {PID_total = 90; } 
-  
-    servo_motor.write(PID_total+30);  
+    if(PID_total < 25){PID_total = 25;}
+    if(PID_total > 120) {PID_total = 120; } 
+
+    if(distance_error < -1 || distance_error > 1){
+      servo_motor.write((int) PID_total+30);  
+    }
     distance_previous_error = distance_error;
 
-    Serial.println(distance_error);
-    Serial.print(",");
-    Serial.println(PID_total + 30);
+    
+    
   }
   
-
 
   
   delay(10); // uncomment this to introduce an additional delay
@@ -124,6 +175,31 @@ delayMicroseconds(10);
 digitalWrite(trigPin1, LOW);
 
 durata = pulseIn(echoPin1, HIGH);
+distanza = (float)durata/58.2;
+
+delay(10);
+
+if (distanza > 42) distanza=43;
+else if (distanza < 0) distanza=0;
+
+return (distanza);   // meters   
+
+}
+
+float measure_2 (void) {
+
+long durata=0;
+float distanza=0; 
+
+digitalWrite(trigPin2, LOW); 
+delayMicroseconds(10); 
+
+digitalWrite(trigPin2, HIGH);
+delayMicroseconds(10);
+ 
+digitalWrite(trigPin2, LOW);
+
+durata = pulseIn(echoPin2, HIGH);
 distanza = (float)durata/58.2;
 
 delay(10);
